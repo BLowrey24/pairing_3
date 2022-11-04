9e773a46e742edcb4660320fe30622e46fe2e915
Boston Lowrey <bostonlowrey@gmail.com>
2022-11-03 21:26:41 -0400

    Add second if

--- friends.rb
+++ friends.rb
@@ -2,5 +2,11 @@ fun_numbers = [1, 13, 24, 50, 100, 45, 79, 33]
     fun_numbers.each do |num|
         if num > 30 
         puts "#{num}"
+       end
+end
+
+  fun_numbers.each do |sixty|
+        if sixty < 60 
+        puts "#{sixty}"
     end
- end
+  end
